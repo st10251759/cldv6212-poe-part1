95fb45ef3c0963576690b1e1115c230ebeb3cb98
chett <chett@CAMSPC0902>
2024-08-16 22:12:50 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10251759_CLDV6212_POE_Par1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
